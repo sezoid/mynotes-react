bf1de869a9d2c4d94bdb18765d611212e265c76e
Roman Sultanov (SZ) <sezyara@gmail.com>
2018-07-01 21:50:00 +0500

    Fix of date when click to copies button

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -10,7 +10,7 @@ const NoteItem = props => (
 		<h1 className='NoteTitle'>{props.title}</h1>
 		<span className='NoteDate'>{Utils.Time(props.date, props.lang)}</span>
 		<p className='NoteText'>{props.text}</p>
-		<CopyToClipboard text={`[${props.date}] ${props.title} \n\n ${props.text}`}>
+		<CopyToClipboard text={`[${Utils.Time(props.date, props.lang)}] ${props.title} \n\n ${props.text}`}>
 			<div className='NoteCopy'>{Language[props.lang].button_copy}</div>
 		</CopyToClipboard>
 		<img alt={Language[props.lang].button_delete} className='NoteDelete' onClick={props.delete} src='/assets/icons/cross.svg' />
